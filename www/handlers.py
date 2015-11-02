28d1d3d3e059211f898292dc82cad52f2e583bb3
maoyu2014 <haczmy@qq.com>
2015-11-02 11:41:58 +0800

    build frontend, add file __base__.html and blogs.html, modify handlers.py

--- www/handlers.py
+++ www/handlers.py
@@ -6,8 +6,9 @@
 # from models import User, Comment, Blog, next_id
 
 import logging
+import time
 from www.coroweb import get
-from www.models import User
+from www.models import User, Blog
 
 __author__ = 'maoyu'
 
@@ -15,8 +16,13 @@ __author__ = 'maoyu'
 
 @get('/')
 def index(request):
-	users = yield from User.findAll()
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+		Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+		Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+	]
 	return {
-		'__template__':'test.html',
-		'users': users
+		'__template__':'blogs.html',
+		'blogs':blogs
 	}
